4c23e3926e63663a9e7c053384343b0eb875f3b2
Matheus Comp <37025685+MComp413@users.noreply.github.com>
2018-06-11 20:35:57 -0300

    Update Neuronio.py
    /----- CHANGELOG NEURONIO BETA 1.1 -----/
    Adições:
    * corrigirNeuronio(self, erro)
    Remoções:
    * derivLogistica(self, x) - o conceito foi utilizado, mas o método não.
    Modificações:
    * setGlia(self, erro) implementada
    * glia agora é escalar, e não lista
    * sinapse(self) renomeada para setAxonio(self) - melhor interpretação do código

--- Code/Pavlov/Neuronio.py
+++ Code/Pavlov/Neuronio.py
@@ -6,7 +6,7 @@ class Neuronio:
     mielina = []    #pesos
     dendritos = []  #entradas
     axonio = 0      #saida
-    glia = []       #passo de correcao
+    glia = 0       #passo de correcao
 
     #metodos:
     def __init__(self, entradas):
@@ -18,20 +18,22 @@ class Neuronio:
     def sinal(self, entrada):
         self.dendritos = entrada
 
-    def sinapse(self):
+    def setAxonio(self):
         self.axonio = self.logistica(self.produtoEscalar(self.dendritos, self.mielina))
 
+    def setGlia(self, erro):
+        self.glia = erro * self.axonio * (1 - self.axonio)
+    
+    def corrigirNeuronio(self, erro):
+        self.setGlia(erro)
+        for index in range(len(self.dendritos)):
+            self.mielina[index] -= self.glia * self.dendrito[index]
+    
     def produtoEscalar(self, vetorA, vetorB):
         resultado = 0
         for i in range(len(vetorA)):
-            resultado = resultado + vetorA[i] * vetorB[i]
+            resultado += vetorA[i] * vetorB[i]
         return resultado
-
+        
     def logistica(self, x):
         return (1/(1 + math.exp(-x)))
-
-    def derivLogistica(self, x):
-        return self.logistica(x) * (1 - self.logistica(x))
-
-    def setGlia(self, erro):
-        pass
