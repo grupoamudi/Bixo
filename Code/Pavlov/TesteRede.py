a75bf0b89c221d871fe04db9462e00c2a0cb2e14
Vinytt <38005211+Vinytt@users.noreply.github.com>
2018-10-03 13:34:28 -0300

    Update TesteRede.py

--- Code/Pavlov/TesteRede.py
+++ Code/Pavlov/TesteRede.py
@@ -3,21 +3,26 @@ import Camada
 import Neuronio
 import CriadorDeRede
 
+#declaracao de neuronios que farao parte da camada 1
 n1 = Neuronio.Neuronio(4)
 n2 = Neuronio.Neuronio(4)
 n3 = Neuronio.Neuronio(4)
 n4 = Neuronio.Neuronio(4)
 
+#declaracao da camada 1
 camada1 = [n1,n2,n3,n4]
 
 cmd1 = Camada.Camada(camada1)
 
+#declaracao de neuronios que farao parte da camada 2
 n5 = Neuronio.Neuronio(4)
 n6 = Neuronio.Neuronio(4)
 n7 = Neuronio.Neuronio(4)
 
+#declaracao da camada 2
 camada2 = [n5,n6,n7]
 
+#declaracao das entradas que alimentarao a camada 1
 entradas = [1,2,3,4]
 
 matriz = [camada1, camada2]
